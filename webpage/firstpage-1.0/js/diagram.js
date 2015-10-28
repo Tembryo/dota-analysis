1b9a9521d11083c944cc29b12d8eee60378ae77c
Quirin Fischer <fischerq.de@gmail.com>
2015-10-28 15:25:55 +0000

    starting to add diagram to page

---
+++ webpage/firstpage-1.0/js/diagram.js
@@ -0,0 +1,44 @@
+visible_players = [];
+
+for(var i = 0; i < 10; ++i)
+	visible_players.push(false);
+
+function updatePlayers(data)
+{
+	d3.select("#viz").selectAll("[player-id]")
+		.data(data)
+		.attr("visibility", function(d){
+				if(d)
+					return "visible";
+				else
+					return "hidden";
+			});
+
+}
+
+function togglePlayer(player)
+{
+	var index = parseInt(player.dataset["id"]);
+	visible_players[index] = !visible_players[index];
+	updatePlayers(visible_players);
+};
+
+d3.xml("diagram.svg", "image/svg+xml", function(xml) {
+    var importedNode = document.importNode(xml.documentElement, true);
+    d3.select("#viz").node().appendChild(importedNode);
+    d3.select("svg").attr({
+    "class": "svg-content",
+    "width": "100%",
+    "height": "100%"
+  });
+	
+	updatePlayers(visible_players);    
+
+
+    d3.selectAll(".player")
+        .on("click", function(){togglePlayer(this)});
+    
+    
+    
+});
+
