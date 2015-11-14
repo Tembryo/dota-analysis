d1f152a61a572978ea0171d67c3e6acbad597317
rm-tembryo <richard.mason@tembryo.com>
2015-11-14 19:24:14 +0000

    first automatically generated json with kills

--- replayAnalyser/Beta/event_extractor.py
+++ replayAnalyser/Beta/event_extractor.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import math
 
 input_filename = "events_replay_data.csv"
 output_filename = "test.json"
@@ -14,15 +15,13 @@ data_list =[]
 #have to be careful because the clarity extractor has a different naming convention for several of the heros
 heros = {"spirit_breaker":"radiant","queenofpain":"radiant", "antimage":"radiant","dazzle":"radiant","dark_seer":"radiant", \
 "undying":"dire","witch_doctor":"dire","necrolyte":"dire","tusk": "dire", "alchemist":"dire"}
+teams = {0:{"side":"radiant","name":"empty","short":"empty"},1:{"side":"dire","name":"empty","short":"empty"}}
+players = {0:{"name":"a"},1:{"name":"b"},2:{"name":"b"},3:{"name":"a"},4:{"name":"b"},5:{"name":"b"},6:{"name":"a"},7:{"name":"b"},8:{"name":"b"},9:{"name":"b"}}
 
-events =[]
-
-header = {"id":0,"draft": {},"length": 600}
-
-events.append(header)
+header = {"id":0,"draft": {},"length": 600,"teams":teams,"players":players}
 
 kills_namespace =10000
-
+events =[]
 k=0
 for i, row in enumerate(reader):
 	# for each row check if a death occurred
@@ -38,7 +37,7 @@ for i, row in enumerate(reader):
 			side = heros[hero_name]
 			# now form a dictionary to s
 			id_num = kills_namespace+k
-			new_event = {str(id_num):{"type":"kill","time":row[0],"team":side}}	
+			new_event = {str(id_num):{"type":"kill","time": math.floor(float(row[0])),"team":side}}	
 			events.append(new_event)
 			k=k+1
 
