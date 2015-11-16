2057dc5af0e4f258cc3d0dd0ae62d6b9739ad0df
rm-tembryo <richard.mason@tembryo.com>
2015-11-16 10:34:11 +0000

    business card

--- replayAnalyser/Beta/event_extractor.py
+++ replayAnalyser/Beta/event_extractor.py
@@ -8,6 +8,8 @@ position_input_filename = "replay_data.csv"
 events_input_filename = "events_replay_data.csv"
 output_filename = "test.json"
 
+#open up the files
+
 e = open(position_input_filename,'rb')
 position_reader = csv.reader(e)
 
@@ -16,19 +18,23 @@ events_reader = csv.reader(f)
 
 g = open(output_filename,'wb')
 
+# count the number of rows
+
+row_count1 = sum(1 for row in position_reader)
+print row_count1
+
 #extract areas for each hero
 
-replay = Dota2Replay(position_input_filename)
-player_data = readPlayerData(replay,2,30000,30000,10)
-#print player1data[1]
+areas_dict = {}
+for i in range(1,2):
+	replay = Dota2Replay(position_input_filename)
+	player_data = readPlayerData(replay,i,100000,row_count1,10)
+	area_state = assignPlayerArea(replay,player_data,area_matrix)
+	summary = areaStateSummary(player_data,area_state)
+	player_summary = {"areas":summary[0],"times":summary[1]}
+	areas_dict[i] = player_summary
 
-area_state = assignPlayerArea(replay,player_data,area_matrix)
-#print area_state
-
-summary = areaStateSummary(player_data,area_state)
-print summary[0]
-print summary[1]
-
+e.close()
 
 # start of the analysis
 
@@ -69,7 +75,6 @@ data_to_write = {"header":header,"events":events}
 
 g.write(json.dumps(data_to_write, sort_keys=False,indent=4, separators=(',', ': ')))
 
-e.close()
 f.close()
 g.close()
 
