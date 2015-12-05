fd9d7e794eadf23955a184fe997855de6ef116ab
Quirin Fischer <fischerq.de@gmail.com>
2015-12-05 08:08:29 +0000

    extracted pw, fixed steam realm

--- web-server/webserver/src/config.js
+++ web-server/webserver/src/config.js
@@ -1,3 +1,4 @@
+var sensitive = require("sensitive_config.js");
 exports.host            = process.env.VIRTUAL_HOST;
 exports.database_host   = "DATABASE_IP:42200";
 exports.files           = "/files";
@@ -5,7 +6,9 @@ exports.storage         = "/storage";
 exports.shared          = "/shared";
 
 exports.port            = process.env.VIRTUAL_PORT;
-if(process.env.DEV_VERSION === "true")
+if(process.env.VERSION === "droplet")
+    exports.steam_realm   = "http://46.101.120.4:80/";
+else if (process.env.VERSION === "DEV")
     exports.steam_realm   = "http://localhost:80/";
 else
     exports.steam_realm   = "http://wisdota.com/";
@@ -13,4 +16,4 @@ else
 exports.steam_api_key   = "738D637E98D73D27B9802CA833784D7F";
 
 exports.gmail_user = "quirin.fischer@tembryo.com";
-exports.gmail_password = "xxxxxxx";
+exports.gmail_password = sensitive.gmail_pw;
