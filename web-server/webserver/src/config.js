3b5690ceb08a815a0d53a1571185ead607456504
Quirin Fischer <fischerq.de@gmail.com>
2015-12-06 16:57:37 +0000

    added www domain

--- web-server/webserver/src/config.js
+++ web-server/webserver/src/config.js
@@ -1,12 +1,13 @@
 var sensitive = require("./sensitive_config.js");
-exports.host            = process.env.VIRTUAL_HOST;
+var host = process.env.VIRTUAL_HOST.split(",")[0];
+exports.host            = host;
 exports.database_host   = "DATABASE_IP:42200";
 exports.files           = "/files";
 exports.storage         = "/storage";
 exports.shared          = "/shared";
 
 exports.port            = process.env.VIRTUAL_PORT;
-exports.steam_realm   = "http://"+process.env.VIRTUAL_HOST+":80/";
+exports.steam_realm   = "http://"+host+":80/";
 
 exports.steam_api_key   = "738D637E98D73D27B9802CA833784D7F";
 
