ac68bd785579858616c8488cf25c1618a4f57193
Quirin Fischer <fischerq.de@gmail.com>
2015-12-06 16:32:04 +0000

    fixed host for droplet/steam realm

--- web-server/webserver/src/config.js
+++ web-server/webserver/src/config.js
@@ -6,12 +6,7 @@ exports.storage         = "/storage";
 exports.shared          = "/shared";
 
 exports.port            = process.env.VIRTUAL_PORT;
-if(process.env.VERSION === "droplet")
-    exports.steam_realm   = "http://46.101.120.4:80/";
-else if (process.env.VERSION === "DEV")
-    exports.steam_realm   = "http://localhost:80/";
-else
-    exports.steam_realm   = "http://wisdota.com/";
+exports.steam_realm   = "http://"+process.env.VIRTUAL_HOST+":80/";
 
 exports.steam_api_key   = "738D637E98D73D27B9802CA833784D7F";
 
