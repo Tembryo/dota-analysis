e8afc4c28eaca873ecaa2b1ebd4694204a3c37f9
rm-tembryo <richard.mason@tembryo.com>
2016-01-25 12:20:00 +0000

    first commit of second version of website

---
+++ webpage/secondpage/js/mycarousel.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+      $('.slider').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+    });
