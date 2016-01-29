8adf78f907f8d4892ad55a5209603d59af2c5b01
rm-tembryo <richard.mason@tembryo.com>
2016-01-29 20:29:10 +0000

    fixed the progress bar so that it is now nice and smooth

--- webpage/secondpage/js/landing.js
+++ webpage/secondpage/js/landing.js
@@ -1,34 +1,29 @@
 $(document).ready(function(){	
-	$("#ratePlayerBtn").click(function(){
-		timeoutID = window.setTimeout(modifyBar1,3000);
-	});
+	$("#progressTimer").progressTimer({
+    timeLimit: 12,
+    warningThreshold: 10,
+    baseStyle: 'progress-bar-warning',
+    warningStyle: 'progress-bar-danger',
+    completeStyle: 'progress-bar-info',
+    onFinish: function() {
+        console.log("I'm done");
+        window.location.replace("user.html")
+    }
+});
 
-	function modifyBar1(){
-		$("#my-loading-bar").attr("style","width:30%").text("30% Complete");
-		$("#my-modal-title").text("Parsing Replay");
-		timeoutID = window.setTimeout(modifyBar2,4000);
-	}
+	// $("#ratePlayerBtn").click(function(){
+	// 	var bar_width = 10
+	// 	while (bar_width <100){
+	// 		timeoutID = window.setTimeout(function(){modifyBar(bar_width);},10000);
+	// 		bar_width = bar_width +1 
+	// 	}
+	// });
 
-	function modifyBar2(){
-		$("#my-loading-bar").attr("style","width:50%").text("50% Complete");
-		$("#my-modal-title").text("Analysing Match");
-		timeoutID = window.setTimeout(modifyBar3,4000);
-
-	}
-
-	function modifyBar3(){
-		$("#my-loading-bar").attr("style","width:80%").text("80% Complete");
-		$("#my-modal-title").text("Calculating MMR");
-		timeoutID = window.setTimeout(modifyBar4,6000);
-
-	}
-
-	function modifyBar4(){
-		$("#my-loading-bar").attr("style","width:100%").text("100% Complete");
-		timeoutID = window.setTimeout(modifyBar5,2000);
-	}
-	function modifyBar5(){
-		window.location.replace("results.html");
-	}
+	// function modifyBar(bar_width){
+	// 	var bar_str = bar_width.toString();
+	// 	var str = "width:" + bar_str + "%"
+	// 	var txt = bar_str + "% Complete"
+	// 	$("#my-loading-bar").attr("style",str).text(txt);
+	// }
 
 });
