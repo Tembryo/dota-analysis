81f3f08556da55661b6e8a73a97a36f1c676b3cf
Quirin Fischer <fischerq.de@gmail.com>
2016-02-11 20:01:54 +0000

    added logout

--- web-server/webserver/src/routes-auth.js
+++ web-server/webserver/src/routes-auth.js
@@ -185,6 +185,15 @@ router.route("/steam/return")
         }
 	);
 
+router.route("/logout")
+    .get(   
+        function(req, res) {
+            console.log("logging out");
+            req.logout();
+            res.redirect('/');
+        }
+    );
+
 // Simple route middleware to ensure user is authenticated.
 //   Use this route middleware on any resource that needs to be protected.  If
 //   the request is authenticated (typically via a persistent login session),
