9d586e71049487d015a551f0fa5d3a0b47f5ecb1
Quirin Fischer <fischerq.de@gmail.com>
2016-07-02 04:27:24 +0100

    dota conneciton changes 1.0

--- web-server/shared-code/dota.js
+++ web-server/shared-code/dota.js
@@ -1,122 +1,126 @@
 var async   = require("async"),
-    Steam   = require('steam'),
+    SteamUser   = require('steam-user'),
     dota2   = require('dota2'),
     domain = require('domain');
 
-var dota_semaphore = require("semaphore")(1);
+var steam_user      = new SteamUser({"dataDirectory": "/storage"});
+var steam_client    = steam_user.client;
+var steam_status    = "Open";
+var dota_client     = new dota2.Dota2Client(steam_client, true, true);
+var dota_status     = "Open";
 
+var queued_dota_calls = [];
+var queueing_block = require("semaphore")(1);
 
-function performDotaAction(login_data, main_call, callback_final)
+var reconnect_timeout = 1000;
+
+function openDotaClient(login_data, callback)
 {
-    var dota_error = function(err)
+    console.log("open called");
+    if((steam_status === "Connected" || dota_status === "Connected"))
     {
-        console.log("dota error", err);
-        callback_final(err);
+        console.log("already connected");
+        callback("already connected");
     }
+    else if (dota_status === "Failed")
+        callback("Dota is Failed");
 
-    var d = domain.create();  
-    d.on('error', dota_error) 
-    d.enter(); 
+    var callback_called = false;
+    dota_client.on("ready", function()
+        {
+            console.log("Dota client started");
+            dota_status = "Connected";
+            if(!callback_called)
+            {
+                callback();
+                callback_called = true;
+            }
+        });
+    dota_client.on("unready", function()
+        {
+            dota_status = "Open";
+            console.log("Dota client lost connection, reconnecting");
+            dota_client.launch();
+        });
 
-    dota_semaphore.take(
-        function(){
-            var steamClient     = new Steam.SteamClient(),
-                steamUser       = new Steam.SteamUser(steamClient),
-                steamFriends    = new Steam.SteamFriends(steamClient),
-                Dota2           = new dota2.Dota2Client(steamClient, true);
+    steam_user.on("loggedOn", function (details, parental){
 
-            async.waterfall(
-                [
-                    function(callback)
-                    {
-                        steamClient.on('connected', callback);
-                        steamClient.on('error', function(err){
-                            console.log("stean client error", err);
-                            callback("steamClient error", err);
-                        });
-                        try{
-                            steamClient.connect();
-                        }
-                        catch(err)
-                        {
-                            console.log("got exception", err);
-                            callback(err);
-                        }
-                    },
-                    function(callback)
-                    {
-                        steamClient.on('logOnResponse', 
-                            function(response)
-                            {
-                                if (response.eresult == Steam.EResult.OK)
-                                    callback();
-                                else
-                                    callback("bad logOn response", response);
-                            });
+        console.log("Steam logged on",details, parental)
+        steam_status = "Connected";
+        steam_user.setPersona(SteamUser.EPersonaState.Busy, "Wisdota Bot");
 
-                        steamClient.on('loggedOff', function(){ callback("steamClient logged off"); });
-                        console.log("logging in using:", login_data);
-                        steamUser.logOn({
-                            account_name: login_data.steam_user,
-                            password: login_data.steam_pw
-                        });
-                    },
-                    function(callback)
-                    {
-                        steamFriends.setPersonaState(Steam.EPersonaState.Busy);
-                        steamFriends.setPersonaName("Wisdota Bot");
-                        console.log("Logged on.");
+        dota_client.launch();
+    });
+    steam_user.on("disconnected", function (eresult, msg){
+        console.log("Steam got disconnected", eresult, msg);
+        steam_status = "Open";
+        if( dota_status === "Connected")
+            dota_client.exit();
 
-                        Dota2.on("ready", callback);
-                        Dota2.launch();
-                    },
-                    function(callback)
-                    {
-                        console.log("Node-dota2 ready.");
-                        d.exit(); 
-                        main_call(Dota2, callback);
-                    },
-                    function()//extracting parameters from arguments array
-                    {   
-                        d.enter(); 
-                        Dota2.on("unready", function() {
-                                    console.log("Node-dota2 unready.");
-                                });
-                        Dota2.exit();
-                        //console.log(result, callback);
-                        var new_arguments = Array.prototype.slice.call(arguments);
-                        //console.log(new_arguments);
-                        var callback = new_arguments.pop();
-                        new_arguments.unshift(null);
+        setTimeout(
+            function(){
+                steam_user.logOn(login_data);
+            },
+            reconnect_timeout
+        );
 
-                        callback.apply(this, new_arguments);
-                    },
-                    function()//extracting parameters from arguments array
-                    {
-                        steamClient.disconnect();
+    });
 
-                        var new_arguments = Array.prototype.slice.call(arguments);
-                        //console.log(new_arguments);
-                        var callback = new_arguments.pop();
-                        new_arguments.unshift(null);
-
-                        callback.apply(this, new_arguments);
-                    }
-                ],
-                function(err, result){
-                    if(err)
-                    {
-                       console.log("Dota action error occured", err, result); 
-                    }
-
-                    dota_semaphore.leave();
-
-                    d.exit(); 
-                    callback_final(err, result);
-                }
-            );
+    steam_user.on("error", function(err) {
+        steam_status = "Failed";
+        console.log("Steam error", err);
+        if(!callback_called)
+        {
+            callback("failed");
+            callback_called = true;
         }
-    );
+        if( dota_status === "Connected")
+            dota_client.exit();
+    });
+
+    steam_user.logOn(login_data);
 }
 
-exports.performAction = performDotaAction;
+
+function closeDotaClient(callback)
+{
+    if(dota_status === "Open")
+        callback();
+    else if(dota_status === "Failed")
+        callback("Dota is Failed");
+
+    dota_client.on("unready", function()
+    {
+        console.log("Dota intentionally closed");
+        dota_status = "Open";
+        steam_user.logOff();
+    });
+
+    var callback_called = false;
+    steam_user.on("disconnected", function (eresult, msg){
+        console.log("Intentionally disconnected steam", eresult, msg);
+        steam_status = "Open";
+        if(!callback_called)
+        {
+            callback_called = true;
+            callback();
+        }
+    });
+
+    dota_client.exit();
+}
+
+function getClient(callback)
+{
+    if(dota_status === "Connected")
+        callback(null, dota_client);
+    else if(dota_status === "Failed")
+        callback("Steam Failed");
+    else if(dota_status === "Open")
+        callback("Not Connected");
+}
+
+exports.openDotaClient = openDotaClient;
+exports.closeDotaClient = closeDotaClient;
+
+exports.getClient = getClient;
