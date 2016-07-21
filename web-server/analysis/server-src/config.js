8c3821d3e60c7e00a38aea7cc3ee572c509d8c07
Quirin Fischer <fischerq.de@gmail.com>
2016-07-21 11:53:15 +0100

    stuff

--- web-server/analysis/server-src/config.js
+++
@@ -1,7 +0,0 @@
-var sensitive = require("./sensitive_config.js");
-exports.database_host   = "POSTGRES_IP";
-exports.database_auth   = "wisdota:"+sensitive.database_pw;
-
-exports.files           = "/files";
-exports.storage         = "/storage";
-exports.shared          = "/shared";
